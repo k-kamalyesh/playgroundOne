7905684af7cc281c2b9c8262094d65278e7a01f0
kamalyesh <kkamalyesh@gmail.com>
2021-03-29 21:32:06 +0530

    webpack config updated to include image files

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,18 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './images',
+              publicPath: './bin/images',
+            },
+          },
+        ],
+      },
     ],
   },
 };
